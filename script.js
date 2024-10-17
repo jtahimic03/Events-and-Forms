c05e8a08365834d7e42d1065c30c53ddb9484a40
Jansen Tahimic <jtahimic@academic.rrc.ca>
2024-10-16 22:51:02 -0500

    added comments to my code

--- script.js
+++ script.js
@@ -3,49 +3,56 @@ const email = document.getElementById('email')
 const form = document.getElementById('photoShootForm')
 const errorElement = document.getElementById('error')
 const radioButtons = document.getElementsByName('setting');
+//global variables
+
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
+    e.preventDefault(); //prevent the form from submmitting with invalid inputs
 
-    validateInputs();
+    validateInputs(); // calls the function when submit button is clicked
 
 })
 
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
+const setError = (element, message) => {//parameters is the html element and its message
+    const inputControl = element.parentElement; //error message will be placed inside the parent element 
+    const errorDisplay = inputControl.querySelector('.error');//where he error message will be displayed
+
+    errorDisplay.innerText = message;//sets the text to the message from the parameter 
+    inputControl.classList.add('error');//adds the error class 
+    inputControl.classList.remove('success')//removes success class if its present 
 }
 
+//vise versa compared to setError
 const setSuccess = element => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
 
-    errorDisplay.innerText = '';
+    errorDisplay.innerText = '';//removes the error text if displayed
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
 };
 
+//checks if the email is valid and in the right format
 const isValidEmail = email => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    return re.test(String(email).toLowerCase());//changes the string to lowercase
 }
 
 const validateInputs = () => {
-    const firstNameValue = firstName.value.trim();
+    //variables used for the function
+    const firstNameValue = firstName.value.trim();//trim removes any spaces user could press
     const emailValue = email.value.trim();
-    if(firstNameValue === ''){
+
+    if(firstNameValue === ''){//checks if the string is empty
         setError(firstName, 'First name is required');
     }else{
         setSuccess(firstName)
     }
 
-    if(emailValue === '') {
+    if(emailValue === '') {//checks if email is empty
         setError(email, 'Email is required');
-    }else if (!isValidEmail(emailValue)) {
+    }else if (!isValidEmail(emailValue)) {//check if email is valid
         setError(email, 'Provide a valid email address');
     }else {
         setSuccess(email);
